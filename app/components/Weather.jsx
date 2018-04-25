f129c524c5036a94a8fd1d2db2aa725fe6a9c3af
Salam Omarbagaev <omarbagaev@outlook.com>
2018-04-25 13:07:57 +1200

    Stable commit for recovery

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -15,7 +15,9 @@ var Weather = React.createClass({
 
         this.setState({
             isLoading: true,
-            errorMessage: undefined
+            errorMessage: undefined,
+            location: undefined,
+            temp: undefined
         });
 
         openWeatherMap.getTemp(location).then(function (temp) {
@@ -31,6 +33,22 @@ var Weather = React.createClass({
             });
         });
     },
+    componentDidMount: function () {
+        var location = this.props.location.query.location;
+
+        if (location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash = '#/';
+        }
+    },
+    componentWillReceiveProps: function (newProps) {
+        var location = newProps.location.query.location;
+
+        if (location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash = '#/';
+        }
+    },
     render: function () {
         var {isLoading, temp, location, errorMessage} = this.state;
 
@@ -45,14 +63,14 @@ var Weather = React.createClass({
         function renderError () {
             if (typeof errorMessage === 'string') {
                 return (
-                    <ErrorModal/>
+                    <ErrorModal message={errorMessage}/>
                 )
             }
         }
 
         return (
             <div>
-                <h1 className="text-center">Get Weather</h1>
+                <h1 className="text-center page-title">Get Weather</h1>
                 <WeatherForm onSearch={this.handleSearch}/>
                 {renderMessage()}
                 {renderError()}
