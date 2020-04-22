31c6ea43f468776947b97118e194c1bb5e6d6dec
Salam Omarbagaev <omarbagaev@gmail.com>
2020-04-23 02:18:21 +1200

    Update WeatherMessage.jsx

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,22 +1,5 @@
 var React = require("react");
 
-// var WeatherMessage = React.createClass({
-//     render: function() {
-//         var {location, temp} = this.props;
-//         //the same like
-//         //var location = this.props.location;
-//         //var temp = this.props.temp;
-//
-//         return (
-//             <div>
-//                 <h3>It's it {temp} in {location}</h3>
-//             </div>
-//         )
-//     }
-// });
-
-//var WeatherMessage = (props) => {
-//var {location, temp} = props;
 var WeatherMessage = ({ temp, location }) => {
   return (
     <div>
