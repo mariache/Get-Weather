8fdb1c27c1153205bdb2c782ebe1156ac0bd15d4
Salam Omarbagaev <omarbagaev@gmail.com>
2020-04-23 02:17:24 +1200

    Update Main.jsx

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,18 +1,6 @@
 var React = require("react");
 var Nav = require("Nav");
 
-// var Main = React.createClass({
-//     render: function () {
-//         return(
-//             <div>
-//                 <Nav/>
-//                 <h2>Get weather react app</h2>
-//                 {this.props.children}
-//             </div>
-//         );
-//     }
-// });
-
 var Main = props => {
   return (
     <div>
