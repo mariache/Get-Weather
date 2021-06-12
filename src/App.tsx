b0f3f9cf69f76cf62f9f2c5bdbdbbc778f147e5f
Salam Omarbagaev <omarbagaev@gmail.com>
2021-06-12 23:20:51 +1200

    - not working ts draft;

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState, FC } from "react";
 
 import "./index.css";
+import { fetchWeather } from "./api/fetchWeather";
+import ReactDOM from "react-dom";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App: FC = () => {
+  const [query, setQuery] = useState<string>("");
+  const [weather, setWeather] = useState<string>("");
+  const onSearch = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+      setWeather(data);
+      setQuery("");
+    }
+  };
 
-ReactDOM.render(<App />, document.getElementById("app"));
+  return (
+    console.log(weather),
+    (
+      <div>
+        <input
+          type="text"
+          name=""
+          id=""
+          className="search"
+          placeholder="Search"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyPress={onSearch}
+        />
+      </div>
+    )
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/api/fetchWeather.ts
@@ -0,0 +1,14 @@
+import { axios } from "axios";
+const BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
+
+export const fetchWeather = async (query) => {
+  const { data } = await axios.get(BASE_URL, {
+    params: {
+      q: query,
+      units: "metric",
+      APPID: process.env,
+    },
+  });
+
+  return data;
+};
